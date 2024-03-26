75c7739d37733e4cf4cc6144183b97789facae4d
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-03-26 15:42:37 +0100

    added image classes, tested image upload/dowload  for single use case

--- gatling/src/test/java/io/gatling/test/rest/RestSimulation.java
+++ gatling/src/test/java/io/gatling/test/rest/RestSimulation.java
@@ -3,25 +3,20 @@ package io.gatling.test.rest;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
+import lombok.val;
 
 import java.time.Duration;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.http.HttpDsl.http;
-import static io.gatling.javaapi.http.HttpDsl.status;
+import static io.gatling.javaapi.http.HttpDsl.*;
+import static scala.Predef.Map;
 
 public class RestSimulation extends Simulation {
     HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8082")
             .acceptHeader("application/json")
             .contentTypeHeader("application/json");
 
-    ScenarioBuilder scn = scenario("Consumer all")
-//            .exec(http("customerAll").get("/customerAll"))
-//            .pause(1)
-//            .exec(http("updateCustomer").post("/updateCustomer/1")
-//                    .body(StringBody("{\"name\": \"test\",\"pesel\": \"test\"}")))
-//            .pause(1)
-//            .exec(http("customerById").get("/customerById/1"));
+    ScenarioBuilder scn = scenario("CRUD consumer")
             .forever().on(
                     http("customerAll").get("/customerAll")
                             .check(status().is(200))
@@ -30,7 +25,6 @@ public class RestSimulation extends Simulation {
                             .check(jsonPath("$[*].pesel").findAll().exists())
                             .check(jsonPath("$[*].name").findAll().exists())
                     ,
-
                     http("updateCustomer").post("/updateCustomer/1")
                         .body(StringBody("{\"name\": \"test\",\"pesel\": \"test\"}"))
                             .check(status().is(200))
@@ -38,21 +32,36 @@ public class RestSimulation extends Simulation {
                     http("addCustomer").post("/addCustomer")
                                     .body(StringBody("{\"name\": \"newUser\",\"pesel\": \"newUser\"}"))
                             .check(status().is(200))
-
                     ,
-
                     http("deleteCustomer").delete("/deleteCustomer/newUser")
                             .check(status().is(200))
+            );
+    ScenarioBuilder scn2 = scenario("CRUD image")
+            .exec(
+                    http("imageUpload").post("/image")
+//                            .bodyPart(RawFileBodyPart("file","src/test/image/testImage.jpg")
+//                                    .contentType("image/jpeg"))
+//                            .formParam("image", RawFileBodyPart("file","src/test/image/testImage.jpg")
+//                                    .contentType("application/jpg"))
+//                            .queryParam("image",RawFileBodyPart("file","src/test/image/testImage.jpg") )
+                            .header("Content-Type", "multipart/form-data")
+                            .bodyPart(RawFileBodyPart("image", "src/test/image/testImage.jpg")
+                                    .contentType("image/jpeg").fileName("testImage.jpg"))
+                            .asMultipartForm()
+                            .check(status().is(200)),
+                    http("imageDowload").get("/image/testImage.jpg")
+                            .check(status().is(200))
+                            .check(bodyBytes().is(RawFileBody("src/test/image/testImage.jpg")))
             );
     {
 
         //constantUsersPerSec(10).during(Duration.ofMinutes(2)
         setUp(
-                scn.injectOpen(
+                scn2.injectOpen(
                         //rampUsersPerSec(5).to(10).during(Duration.ofSeconds(120))
                         //rampUsersPerSec(50).to(200).during(Duration.ofSeconds(90))
                         //constantUsersPerSec(200).during(Duration.ofMinutes(2))
-                        atOnceUsers(2)
+                        atOnceUsers(1)
                         )
         ).protocols(httpProtocol).maxDuration(Duration.ofSeconds(5));
     }
