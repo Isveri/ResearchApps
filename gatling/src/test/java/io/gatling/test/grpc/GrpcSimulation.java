6a7d007c972b1f2ebf9f30443cb77b99f6fe9dcb
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-03-29 16:04:00 +0100

    gatling grpc test for customer

--- gatling/src/test/java/io/gatling/test/grpc/GrpcSimulation.java
+++ gatling/src/test/java/io/gatling/test/grpc/GrpcSimulation.java
@@ -1,41 +1,63 @@
 package io.gatling.test.grpc;
 
-import com.google.protobuf.Empty;
-import com.google.protobuf.Int32Value;
+import com.google.protobuf.*;
+import io.gatling.grpc.check.GrpcCheckScope;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.grpc.GrpcProtocolBuilder;
 import io.grpc.Status;
+
+import pl.piomin.services.grpc.customer.model.CustomerProto;
 import pl.piomin.services.grpc.customer.model.CustomersServiceGrpc;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.grpc.GrpcDsl.*;
 public class GrpcSimulation extends Simulation {
 
+    AtomicInteger counter = new AtomicInteger(0);
     GrpcProtocolBuilder baseGrpcProtocol = grpc.forAddress("localhost", 9092).useInsecureTrustManager();
 
-    ScenarioBuilder unaryFindAll = scenario("Consumer unary")
-            .forever().on(exec(grpc("findAll")
+    ScenarioBuilder scn1 = scenario("Consumer unary")
+            .forever().on(
+                    exec(session -> session.set("my_var", counter.getAndIncrement())),
+                    grpc("findAll")
                     .unary(CustomersServiceGrpc.getFindAllMethod())
                     .send(Empty.newBuilder().build())
-                    .check(
-                            statusCode().is(Status.Code.OK)
+                    .check(statusCode().is(Status.Code.OK)
+                            //,response(CustomerProto.Customers::getCustomersList).is()
                     )
-            ));
-    ScenarioBuilder unaryFindById = scenario("Find by id unary")
-            .exec(grpc("findById")
-                    .unary(CustomersServiceGrpc.getFindByIdMethod())
-                    .send(Int32Value.of(1))
-                    .check(
-                            statusCode().is(Status.Code.OK)
-                    )
+                    ,
+                    grpc("addCustomer")
+                            .unary(CustomersServiceGrpc.getAddCustomerMethod())
+                            .send(session -> CustomerProto.Customer.newBuilder()
+                                     .setName("newUser")
+                                     .setPesel("newUser"+session.get("my_var"))
+                                     .build()
+                            )
+                            .check(statusCode().is(Status.Code.OK))
+                    ,
+                    grpc("updateCustomer")
+                            .unary(CustomersServiceGrpc.getUpdateCustomerMethod())
+                            .send(session -> CustomerProto.Customer.newBuilder()
+                                    .setName("newUserChanged")
+                                    .setPesel("newUser"+session.get("my_var"))
+                                    .build()
+                            )
+                            .check(statusCode().is(Status.Code.OK))
+                    ,
+                    grpc("deleteCustomer")
+                            .unary(CustomersServiceGrpc.getDeleteCustomerMethod())
+                            .send(session->StringValue.newBuilder().setValue("newUser"+session.get("my_var")).build())
+                            .check(statusCode().is(Status.Code.OK))
             );
+            // rozwiazanie w docsach Gatling zwiazane z sprawdzaniem odpowiedzi zapytania nie dziala, nie znajduje klasy ResponseMessage
+
     {
-        ScenarioBuilder  scn=unaryFindAll;
-        setUp(scn.injectOpen(atOnceUsers(5)))
-                .protocols(baseGrpcProtocol).maxDuration(Duration.ofMinutes(1));
+        setUp(scn1.injectOpen(atOnceUsers(5)))
+                .protocols(baseGrpcProtocol).maxDuration(Duration.ofSeconds(120));
     }
 
 }
