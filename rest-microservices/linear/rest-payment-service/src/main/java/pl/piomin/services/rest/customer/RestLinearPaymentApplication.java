1c4060665d75aba27017c2d388a4aa41bc690187
Evistix <mephistakaiwan@gmail.com>
2024-05-17 18:30:51 +0200

    fix file loading

--- rest-microservices/linear/rest-payment-service/src/main/java/pl/piomin/services/rest/customer/RestLinearPaymentApplication.java
+++ rest-microservices/linear/rest-payment-service/src/main/java/pl/piomin/services/rest/customer/RestLinearPaymentApplication.java
@@ -2,6 +2,13 @@ package pl.piomin.services.rest.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import pl.piomin.services.rest.customer.model.Image;
+import pl.piomin.services.rest.customer.utils.ImageUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @SpringBootApplication
 public class RestLinearPaymentApplication {
--- rest-microservices/linear/rest-payment-service/src/main/java/pl/piomin/services/rest/customer/bootData/BootData.java
+++ rest-microservices/linear/rest-payment-service/src/main/java/pl/piomin/services/rest/customer/bootData/BootData.java
@@ -5,6 +5,7 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 import pl.piomin.services.rest.customer.model.Image;
 import pl.piomin.services.rest.customer.repository.ImageRepository;
@@ -15,6 +16,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
@@ -26,23 +28,16 @@ public class BootData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        byte[] content = null;
-        try {
-            content = Files.readAllBytes(Paths.get("testImage.jpg"));
-        } catch (final IOException e) {
-        }
         uploadImage();
         System.out.println("___add initial user___");
     }
 
     public void uploadImage() throws IOException {
-        File image = new File("../model/image/testImage.jpg");
         var imageToSave = Image.builder()
                 .name("testImage.jpg")
                 .type("image/jpeg")
                 .imageData(ImageUtils.compressImage(
-                                Files.readAllBytes(image.toPath()))
+                                Files.readAllBytes(ResourceUtils.getFile("classpath:testImage.jpg").toPath()))
                 )
                 .build();
         imageRepository.save(imageToSave);
